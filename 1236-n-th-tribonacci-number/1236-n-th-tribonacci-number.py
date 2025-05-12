38c334911793bc86d6159fa482d8381cff1a0435
SmitVaghasiya <71071525+SmitNVaghasiya@users.noreply.github.com>
2025-05-12 09:25:16 +0530

    Time: 0 ms (100%), Space: 17.9 MB (8.34%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -1,15 +1,12 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-        elif n == 2:
-            return 1
-            
         a = 0
         b = 1
         c = 1
+        if n == 0:
+            return a
+        elif n <= 2:
+            return b
         for i in range(3, n+1):
             d = a + b + c
             a = b
