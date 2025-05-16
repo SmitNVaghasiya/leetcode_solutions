a5ab57edf45ff755087bde376ab463e5ec780b17
SmitVaghasiya <71071525+SmitNVaghasiya@users.noreply.github.com>
2025-05-16 08:49:34 +0530

    Time: 0 ms (100%), Space: 19 MB (36.87%) - LeetHub

--- 1476-count-negative-numbers-in-a-sorted-matrix/1476-count-negative-numbers-in-a-sorted-matrix.py
+++ 1476-count-negative-numbers-in-a-sorted-matrix/1476-count-negative-numbers-in-a-sorted-matrix.py
@@ -2,9 +2,8 @@ class Solution:
     def countNegatives(self, grid: List[List[int]]) -> int:
         count = 0
         for i in range(len(grid)):
-            for j in range(len(grid[0]) - 1, -1, -1):
+            for j in range(len(grid[0])):
                 if(grid[i][j] < 0):
-                    count += 1
-                else:
+                    count = count + (len(grid[0]) - j)
                     break
         return count
